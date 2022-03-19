c40f12ef104562d5380522aa70752ec52d4aecdf
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2022-03-19 14:13:00 +0100

    show virtual level

--- src/lib/drawStats.js
+++ src/lib/drawStats.js
@@ -1,11 +1,11 @@
-import { skillNames } from './skillNames'
+import { skillNames } from "./skillNames";
 
 function getIcon(name) {
 	return new Promise((resolve) => {
-		const image = new Image()
-		image.onload = () => resolve(image)
-		image.src = `/static/img/icons/${name}.png`
-	})
+		const image = new Image();
+		image.onload = () => resolve(image);
+		image.src = `/static/img/icons/${name}.png`;
+	});
 }
 
 const drawOrder = [
@@ -41,16 +41,16 @@ const drawOrder = [
 		skillNames.farming,
 		skillNames.summoning,
 	],
-]
+];
 
 export async function drawStats(ctx, width, height, stats) {
 	// Clear canvas
-	ctx.clearRect(0, 0, width, height)
+	ctx.clearRect(0, 0, width, height);
 
 	// Draw background with top border
-	const backgroundMargin = 10
-	const backgroundPadding = 10
-	ctx.fillStyle = '#BCC0C0'
+	const backgroundMargin = 10;
+	const backgroundPadding = 10;
+	ctx.fillStyle = "#BCC0C0";
 	roundRect(
 		ctx,
 		backgroundMargin,
@@ -58,14 +58,14 @@ export async function drawStats(ctx, width, height, stats) {
 		width - 2 * backgroundMargin,
 		height - 2 * backgroundMargin,
 		10
-	)
-	ctx.fill()
+	);
+	ctx.fill();
 
-	ctx.shadowColor = 'black'
-	ctx.shadowBlur = 8
-	ctx.shadowOffsetX = 2
-	ctx.shadowOffsetY = 2
-	ctx.fillStyle = '#2C343F'
+	ctx.shadowColor = "black";
+	ctx.shadowBlur = 8;
+	ctx.shadowOffsetX = 2;
+	ctx.shadowOffsetY = 2;
+	ctx.fillStyle = "#2C343F";
 	// ctx.fillRect(padding, padding, width - 2 * padding, height - 2 * padding)
 	bgRect(
 		ctx,
@@ -74,76 +74,79 @@ export async function drawStats(ctx, width, height, stats) {
 		width - 2 * backgroundMargin,
 		height - 2 * backgroundMargin - backgroundPadding,
 		10
-	)
-	ctx.fill()
-	ctx.shadowColor = 'transparent'
-	ctx.shadowOffsetX = 0
-	ctx.shadowOffsetY = 0
-	ctx.shadowBlur = 0
+	);
+	ctx.fill();
+	ctx.shadowColor = "transparent";
+	ctx.shadowOffsetX = 0;
+	ctx.shadowOffsetY = 0;
+	ctx.shadowBlur = 0;
 
 	// Draw character name
-	const topBarHeight = 40
-	ctx.fillStyle = 'rgb(245, 245, 245)'
-	ctx.font = '28px Inter'
-	ctx.textAlign = 'center'
+	const topBarHeight = 40;
+	ctx.fillStyle = "rgb(245, 245, 245)";
+	ctx.font = "28px Inter";
+	ctx.textAlign = "center";
 	ctx.fillText(
 		stats.name,
 		width / 2,
 		backgroundMargin + backgroundPadding + topBarHeight - 10
-	)
+	);
 
 	// Draw icons
-	const iconSize = 40
-	const iconMargin = 10
-	const textPadding = 46
+	const iconSize = 40;
+	const iconMargin = 10;
+	const textPadding = 46;
 	for (let columnIndex = 0; columnIndex < drawOrder.length; columnIndex++) {
-		const column = drawOrder[columnIndex]
+		const column = drawOrder[columnIndex];
 		for (let skillIndex = 0; skillIndex < column.length; skillIndex++) {
-			const skill = column[skillIndex]
-			const icon = await getIcon(skill)
+			const skill = column[skillIndex];
+			const icon = await getIcon(skill);
 			const x =
 				backgroundMargin +
 				iconMargin +
 				columnIndex * (iconSize + iconMargin) +
-				columnIndex * textPadding
+				columnIndex * textPadding;
 			const y =
 				backgroundMargin +
 				backgroundPadding +
 				topBarHeight +
 				iconMargin +
-				skillIndex * (iconSize + iconMargin)
-			ctx.textAlign = 'start'
-			ctx.drawImage(icon, x, y, iconSize, iconSize)
-			ctx.fillStyle = stats.skills[skill].level >= 99 ? 'rgb(229, 174, 103)' : 'rgb(245, 245, 245)'
-			ctx.font = '20px Inter'
-			ctx.fillText(stats.skills[skill].level, x + 50, y + 25)
+				skillIndex * (iconSize + iconMargin);
+			ctx.textAlign = "start";
+			ctx.drawImage(icon, x, y, iconSize, iconSize);
+			ctx.fillStyle =
+				stats.skills[skill].level >= 99
+					? "rgb(229, 174, 103)"
+					: "rgb(245, 245, 245)";
+			ctx.font = "20px Inter";
+			ctx.fillText(stats.skills[skill].virtualLevel, x + 50, y + 25);
 		}
 	}
 }
 
 function bgRect(ctx, x, y, width, height, radius) {
-	if (width < 2 * radius) radius = width / 2
-	if (height < 2 * radius) radius = height / 2
-	ctx.beginPath()
-	ctx.moveTo(x, y)
-	ctx.lineTo(x + width, y, x + width, y + height)
-	ctx.arcTo(x + width, y + height, x, y + height, radius)
-	ctx.arcTo(x, y + height, x, y, radius)
-	ctx.lineTo(x, y, x + width, y)
-	ctx.closePath()
+	if (width < 2 * radius) radius = width / 2;
+	if (height < 2 * radius) radius = height / 2;
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	ctx.lineTo(x + width, y, x + width, y + height);
+	ctx.arcTo(x + width, y + height, x, y + height, radius);
+	ctx.arcTo(x, y + height, x, y, radius);
+	ctx.lineTo(x, y, x + width, y);
+	ctx.closePath();
 }
 
 // Source: https://codepen.io/simon-wu/pen/ExgLEXQ
 function roundRect(ctx, x, y, width, height, radius) {
-	if (width < 2 * radius) radius = width / 2
-	if (height < 2 * radius) radius = height / 2
-	ctx.beginPath()
-	ctx.moveTo(x + radius, y)
-	ctx.arcTo(x + width, y, x + width, y + height, radius)
-	ctx.arcTo(x + width, y + height, x, y + height, radius)
-	ctx.arcTo(x, y + height, x, y, radius)
-	ctx.arcTo(x, y, x + width, y, radius)
-	ctx.closePath()
+	if (width < 2 * radius) radius = width / 2;
+	if (height < 2 * radius) radius = height / 2;
+	ctx.beginPath();
+	ctx.moveTo(x + radius, y);
+	ctx.arcTo(x + width, y, x + width, y + height, radius);
+	ctx.arcTo(x + width, y + height, x, y + height, radius);
+	ctx.arcTo(x, y + height, x, y, radius);
+	ctx.arcTo(x, y, x + width, y, radius);
+	ctx.closePath();
 }
 
 // rgb(245, 245, 245)
